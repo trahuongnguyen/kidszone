e7f3a50279e8244845cd4b4d03aef4f6cbc85b2d
nguyenhuongtra <tra.nh.1905@aptechlearning.edu.vn>
2023-05-03 15:50:39 +0700

    commit

---
+++ inc/slider.php
@@ -0,0 +1,8 @@
+<div>
+    <div class="container-fluid text-center mt-3 mb-0" style="background-color: #9ac9db;">
+        Welcome to Kids Zone
+    </div>
+    <div class="container text-center">
+        <img src="../img/slider/slider1.jpg" alt="" style="width: 100%;">
+    </div>
+</div>
