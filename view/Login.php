7b89a08d3c32cb9e41eee38faa12bf3c7ab41a3f
nguyenhuongtra <tra.nh.1905@aptechlearning.edu.vn>
2023-05-03 14:47:48 +0700

    register_login

---
+++ view/Login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/regis_login.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <h1>Login</h1>
+            <table>
+            <tr>
+                <td><label for="uname">Username</label></td>
+                <td><input type="text" name = "uname" value = ""></td>
+            </tr>
+            <tr>
+                <td><label for="pword">Password</label></td>
+                <td><input type="password" name = "pword" value = ""></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name = "login" value = "Login">
+                <a href="Register.php">Register</a>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ view/index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="container-fluid bg_header">
+            <div class="row">
+                <div class="col-md-3 col-12 margin_header">
+                    <img src="../img/logo/welcome.png" class="img-fluid" alt="" width="40%">
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-4 col-12 margin_header">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="button-addon2">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-3 margin_header">
+                    <div class="row">
+                        <div class="col">
+                            <a href="#" class="nav-link fs-5"><i class="fa-solid fa-user"></i> Login</a>
+                        </div>
+                        <div class="col">
+                            <a href="#" class="nav-link fs-5"><i class="fa-solid fa-user-plus"></i> Register</a>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg">
+                <ul class="navbar-nav nav-menu margin_header">
+                    <div class="row">
+                        <li class="menu-item li_nav border-0 col">
+                            <a class="nav-link fs-5" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="menu-item dropdown col li_nav">
+                            <a class="nav-link fs-5" href="#" id="navbarDropdown" data-bs-toggle="dropdown">
+                                Category
+                            </a>
+                            <ul class="dropdown-content" aria-labelledby="navbarDropdown">
+                              <li><a class="nav-link" href="#">Action</a></li>
+                              <li><a class="nav-link" href="#">Another action</a></li>
+                              <li><a class="nav-link" href="#">Something else here</a></li>
+                            </ul>
+                          </li>            
+                        <li class="menu-item li_nav col">
+                            <img src="../img/logo/h.png" alt="" width="50px">
+                        </li>
+                        <li class="menu-item li_nav col">
+                            <a class="nav-link fs-5" href="#">Game</a>
+                        </li>
+                        <li class="menu-item li_nav col">
+                            <a class="nav-link fs-5" href="#">Contact</a>
+                        </li>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div class="container-fluid text-center mt-3 mb-0" style="background-color: #9ac9db;">
+        Welcome to Kids Zone
+    </div>
+    <main>
+        <div class="container text-center">
+            <img src="../img/slider/slider1.jpg" alt="" style="width: 100%;">
+        </div>
+        <div class="container mt-4">
+            <div class="container text-center">
+                <h1>Character</h1>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+            </div>  
+        </div>
+        <div class="container mt-4">
+            <div class="container text-center">
+                <h1>Numberic</h1>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+            </div>  
+        </div>
+        <div class="container mt-4">
+            <div class="container text-center">
+                <h1>Animals</h1>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12 container-fluid">
+                    <img src="../img/character/a.jpg" alt="" width="100%;">
+                    <br>
+                    <br>
+                    <h5 class="container text-center">DỊCH VỤ THẤU HIỂU</h5>
+                    <span>Các dịch vụ tiệc và hội nghị trọn gói theo chuẩn mực cao cấp, chỉn chu cùng mức chi phí hợp lý giúp giảm tải gánh nặng tổ chức, cho phép bạn tập trung hơn ở việc trải nghiệm và tận hưởng</span>
+                </div>
+            </div>  
+        </div>
+    </main>
+    <footer>
+        <div class="container-fluid bg_footer mt-5">
+            <div class="footer--decor"></div>
+            <div class="row container mt-4 ms-auto">
+                <div class="col-md-8 col-sm-12">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-12">
+                            <ul>
+                                <li><h4><a href="#" class="text-decoration-none text-dark"><b>HOME</b></a></h4></li>
+                                <li><a href="#" class="text-decoration-none text-dark">CATEGORY</a></li>
+                                <li><a href="#" class="text-decoration-none text-dark">GAME</a></li>
+                                <li><a href="#" class="text-decoration-none text-dark">CONTACT</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+                            <h4><a href="#" class="text-decoration-none text-dark">CONTACT US</a></h4>
+                            <address>
+                                Website: <a href="https://kidzone.edu.vn">https://kidzone.edu.vn</a><br>
+                                Hotline: <a href="tel:02862873708">(028) 6287 3708</a><br>
+                                <a href="tel:02862873709">(028) 6287 3709</a><br>
+                                Address: 15 Đ. Số 11, Thảo Điền, Quận 2, TPHCM<br>
+                                Email: <a href="mailto:kidzoneintercampus@gmail.com">kidzoneintercampus@gmail.com</a>
+                            </address>
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+                            <h4>FOLLOW US</h4>
+                            <ul class="d-block">
+                                <li class="row">
+                                    <div class="col">
+                                        <a href="" class="fs-1"><i class="fa-brands fa-facebook fa-beat"></i></a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="" class="fs-1"><i class="fab fa-instagram fa-beat"></i></a>
+                                    </div>
+                                </li>
+                                <li class="row">
+                                    <div class="col">
+                                        <a href="" class="fs-1"><i class="fa-brands fa-telegram fa-beat"></i></a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="" class="fs-1"><i class="fa-brands fa-twitter fa-beat"></i></a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <ul>
+                        <li>LOCATION</li>
+                        <li><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15676.341035867361!2d106.73530244618274!3d10.80478190822103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527db0ccd4737%3A0x1244c0359565b7b0!2sKidzone%20International%20Kindergarten!5e0!3m2!1svi!2s!4v1682702474435!5m2!1svi!2s" width="350" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="container">
+                <hr>
+                <span class="text-light">© 2018 VN Works Co., Ltd. All rights reserved.</span>
+            </div>
+            <br>
+        </div>
+    </footer>
+</body>
+</html>
